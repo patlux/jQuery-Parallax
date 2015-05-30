7843ad05cec467f8f3912d2753cf4a87bf8528b7
smart <alexprodvig@gmail.com>
2015-05-30 18:37:14 +0300

    Performance improvement

--- scripts/jquery.parallax-1.1.5.js
+++ scripts/jquery.parallax-1.1.5.js
@@ -1,15 +1,15 @@
 /*
-Plugin: jQuery Parallax
-Version 1.1.5
-Author: ADG Idea Alessandro del Gobbo (based on code of Ian Lunn)
-Twitter: @alexoverflow
-Author URL: http://www.adg-idea.com/
-Project on GitHub: https://github.com/aledelgo/jQuery-Parallax
+ Plugin: jQuery Parallax
+ Version 1.1.5
+ Author: ADG Idea Alessandro del Gobbo (based on code of Ian Lunn)
+ Twitter: @alexoverflow
+ Author URL: http://www.adg-idea.com/
+ Project on GitHub: https://github.com/aledelgo/jQuery-Parallax
 
-Dual licensed under the MIT and GPL licenses:
-http://www.opensource.org/licenses/mit-license.php
-http://www.gnu.org/licenses/gpl.html
-*/
+ Dual licensed under the MIT and GPL licenses:
+ http://www.opensource.org/licenses/mit-license.php
+ http://www.gnu.org/licenses/gpl.html
+ */
 
 // USAGE:
 // .parallax(xPosition, speedFactor, outerHeight) options:
@@ -20,60 +20,60 @@ http://www.gnu.org/licenses/gpl.html
 /*$('#intro').parallax("50%", null 0.1);
  $('#second').parallax("50%", 30, 0.1);*/
 
-(function( $ ){
+(function ($) {
 	var $window = $(window),
 		windowHeight = $window.height();
+
+	function setElementsBackgroundPosition(elements, params) {
+		params.windowScrollTop = $window.scrollTop();
+		params.firstTop = elements.offset().top;
+
+		for (var i = 0; i < elements.length; i += 1) {
+			setElementBackgroundPosition($(elements[i]), params);
+		}
+	}
+
+	function setElementBackgroundPosition(element, params) {
+		var top = element.offset().top,
+			height = params.getHeight(element);
+
+		// Check if totally above or totally below viewport
+		if (top + height < params.windowScrollTop || top > params.windowScrollTop + windowHeight) {
+			return;
+		}
+
+		element.css('backgroundPosition', params.xpos + ' ' + Math.round(params.ypos + (params.firstTop - params.windowScrollTop) * params.speedFactor) + 'px');
+	}
 
 	$window.bind('resize', function () {
 		windowHeight = $(this).height();
 	});
 
-	$.fn.parallax = function(xpos, ypos, speedFactor, outerHeight) {
-		var $this = $(this),
-			getHeight,
-			firstTop;
+	$.fn.parallax = function (xpos, ypos, speedFactor, outerHeight) {
+		var params = {};
 
-		// setup defaults if arguments aren't specified
-		xpos = xpos || "50%";
-		ypos = ypos || 0;
-		speedFactor = speedFactor || 0.5;
+		params.xpos = xpos || '50%';
+		params.ypos = ypos || 0;
+		params.speedFactor = speedFactor || 0.5;
 
-		if (typeof outerHeight === 'undefined')  {
+		if (typeof outerHeight === 'undefined') {
 			outerHeight = true;
 		}
 
 		if (outerHeight) {
-			getHeight = function(jqo) {
-				return jqo.outerHeight(true);
+			params.getHeight = function (element) {
+				return element.outerHeight(true);
 			};
+
 		} else {
-			getHeight = function(jqo) {
-				return jqo.height();
+			params.getHeight = function (element) {
+				return element.height();
 			};
-		}
-
-		function setBackgroundPosition(){
-			var pos = $window.scrollTop();
-
-			$this.each(function(){
-				var $element = $(this),
-					top = $element.offset().top,
-					height = getHeight($element);
-
-				firstTop = $this.offset().top;
-
-				// Check if totally above or totally below viewport
-				if (top + height < pos || top > pos + windowHeight) {
-					return;
-				}
-
-				$this.css('backgroundPosition', xpos + " " + Math.round(ypos + (firstTop - pos) * speedFactor) + "px");
-			});
 		}
 
 		//get the starting position of each element to have parallax applied to it
-		setBackgroundPosition();
+		setElementsBackgroundPosition(this, params);
 
-		$window.bind('scroll resize', setBackgroundPosition);
+		$window.bind('scroll resize', setElementsBackgroundPosition.bind(null, this, params));
 	};
 })(jQuery);
